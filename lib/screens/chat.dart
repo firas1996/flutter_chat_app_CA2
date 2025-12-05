52c2104541d99ebb25f81774dd7b3b07ba7896c6
firas1996 <firas.hm.1996@gmail.com>
2025-12-05 18:33:35 +0100

    ss

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,12 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Chat extends StatelessWidget {
   const Chat({super.key});
-
+  // final _auth = FirebaseAuth.instance;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Chat')),
+      appBar: AppBar(
+        title: const Text('Chat'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            icon: Icon(Icons.exit_to_app, color: Colors.deepPurple, size: 32),
+          ),
+        ],
+      ),
       body: const Center(child: Text('Chat Screen')),
     );
   }
