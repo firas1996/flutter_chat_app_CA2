a9cad315accea0de8cbffb4334932c523996a505
firas1996 <firas.hm.1996@gmail.com>
2025-12-05 20:03:31 +0100

    df

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -1,9 +1,39 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class Chat extends StatelessWidget {
+class Chat extends StatefulWidget {
   const Chat({super.key});
   // final _auth = FirebaseAuth.instance;
+  @override
+  State<Chat> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<Chat> {
+  final _msgController = TextEditingController();
+
+  @override
+  void dispose() {
+    _msgController.dispose();
+    super.dispose();
+  }
+
+  void sendMessage() {
+    final msg = _msgController.text;
+    if (msg.trim().isEmpty) {
+      return;
+    }
+    print('dfdsf');
+    final userId = FirebaseAuth.instance.currentUser!.uid;
+    FirebaseFirestore.instance.collection('messages').add({
+      'text': msg,
+      'createdAt': Timestamp.now(),
+      'userId': userId,
+      'username': 'Firas',
+    });
+    _msgController.clear();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +48,32 @@ class Chat extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(child: Text('Chat Screen')),
+      body: Column(
+        children: [
+          Expanded(child: const Center(child: Text('Chat Messages Here'))),
+          Container(
+            padding: const EdgeInsets.all(15),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _msgController,
+                    decoration: const InputDecoration(
+                      labelText: 'Send a message...',
+                    ),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.send),
+                  onPressed: () {
+                    sendMessage();
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
